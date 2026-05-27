30e44fba23dc2480380b52e437ffa39ea22d259a
Arumugam J <sixface@Neon.local>
2026-05-27 06:54:12 -0700

    Include JS sourcemap

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => ({
     },
     build: {
         chunkSizeWarningLimit: 1000,
+        sourcemap: true,
         rollupOptions: {
             output: {
                 manualChunks(id) {
